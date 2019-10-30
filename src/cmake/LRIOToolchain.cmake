05883a1810aafede8fc810ae88544364a4a9736a
Michael Allwright <allsey87@gmail.com>
2019-10-30 17:37:56 +0100

    Changed the configuration of the build system to use Node.js for testing

--- src/cmake/LRIOToolchain.cmake
+++ src/cmake/LRIOToolchain.cmake
@@ -1,15 +1,19 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR wasm)
 
-find_program(CMAKE_C_COMPILER
-   NAMES emcc
-   PATHS /usr/lib/emscripten ENV PATH)
-
 find_program(CMAKE_CXX_COMPILER
    NAMES em++
    PATHS /usr/lib/emscripten ENV PATH)
 
-find_program(EMSCRIPTEN_SERVER
-   NAMES emrun
-   PATHS /usr/lib/emscripten ENV PATH)
-   
+if(NOT CMAKE_CXX_COMPILER)
+   message(FATAL_ERROR "Could not find the Emcripten compiler")
+endif(NOT CMAKE_CXX_COMPILER)
+
+find_program(NODEJS_RUNTIME
+   NAMES node
+   DOC "Node.js interpreter")
+
+if(NOT NODEJS_RUNTIME)
+   message(FATAL_ERROR "Could not find the Node.js runtime")
+endif(NOT NODEJS_RUNTIME)
+
